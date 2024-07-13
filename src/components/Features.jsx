83adfbd104730dca397509b153c89af5bb8a1c25
Aegox <diegoamundaray2017@gmail.com>
2024-07-12 21:17:05 -0500

    add styles and animation of Features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,39 +1,64 @@
 import styles from '../styles/Features.module.css'
 import background from  '../assets/features-background.jpg'
+import person from  '../assets/person.png'
+import Cog from  '../icons/Cog.jsx'
+import Layers from  '../icons/Layers.jsx'
+import Rocket from  '../icons/Rocket.jsx'
+import Laptop from  '../icons/Laptop.jsx'
+
 
 function Features () {
   return (
     <main className={styles.main}>
       <img src={background} alt="" />
+      <div className={styles.container}> 
       <header>
-        <h1>SOME FEATURES</h1>
+        <h2>SOME FEATURES</h2>
         <div className={styles.line}>
         </div>
-        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dignissimos!
-Lorem ipsum dolor sit amet, consectetur.</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dignissimos!
+Lorem ipsum dolor sit amet, consectetur.</p>
       </header>
-      <section>
+        <div className={styles.articles}>
         <article>
-          <img src={""} alt="" />
-          <h3> </h3>
-          <h4></h4>
+          <div className={styles.divIcon}>
+            <Rocket width="25px" height="25px" color="black" />
+          </div>
+          <div className={styles.item}>
+            <h4>BOOTSTRAP 4 BASES</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
         </article>
         <article>
-          <img src={""} alt="" />
-          <h3></h3>
-          <h4> </h4>
+          <div className={styles.divIcon}>
+            <Cog width="25px" height="25px" color="black" />
+          </div>
+          <div className={styles.item}>
+            <h4>FULLY RESPONSIVE</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+          </div>
         </article>
         <article>
-          <img src={""} alt="" />
-          <h3> </h3>
-          <h4> </h4>
+          <div className={styles.divIcon}>
+            <Layers width="25px" height="25px" color="black" />
+          </div>
+          <div className={styles.item}>
+            <h4>PARALLAX BACKGROUND</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+          </div>
         </article>
         <article>
-          <img src={""} alt="" />
-          <h3> </h3>
-          <h4> </h4>
+          <div className={styles.divIcon}>
+            <Laptop width="25px" height="25px" color="black" />
+          </div>
+          <div className={styles.item}>
+            <h4>EASY TO CUSTOMIZE</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+          </div>
         </article>
-      </section>
+        </div>
+        <img src={person} alt="" />
+      </div> 
     </main>
   )
 }
