f0bf18b408e700cb1ab878bb1af575055462038f
Aegox <diegoamundaray2017@gmail.com>
2024-07-14 11:46:36 -0500

    add Counts.jsx finish (not-animation)

--- src/components/Counts.jsx
+++ src/components/Counts.jsx
@@ -1,13 +1,32 @@
 import styles from '../styles/Counts.module.css'
 import counterBackground from '../assets/counter.jpg'
+import Clock from '../icons/Clock.jsx'
+import Briefcase from '../icons/Briefcase.jsx'
+import Heart from '../icons/Heart.jsx'
+import User from '../icons/User.jsx'
 
 function Counts () {
   return (
     <main className={styles.main}>
-      <img src={counterBackground} alt=""/>
       <article>
-        <span></span>
-        <h4></h4>
+        <Clock color="white" width="53px" height="53px"/>
+        <span>1589</span>
+        <h4>Working Hours</h4>
+      </article>
+      <article>
+        <Briefcase color="white" width="53px" height="53px"/>
+        <span>699</span>
+        <h4>Completed Proyects</h4>
+      </article>
+      <article>
+        <User fill="white" width="45px" height="45px"/>
+        <span>202</span>
+        <h4>No. of Clients</h4>
+      </article>
+      <article>
+        <Heart fill="white" width="45px" height="45px"/>
+        <span>1689</span>
+        <h4>Peoples Love</h4>
       </article>
     </main>
   )
