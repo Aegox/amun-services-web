491afdc65f075caba6d0a948c4964d5138ee178c
aegox <diegoamundaray2017@gmail.com>
2024-07-22 22:42:10 -0400

    fix team item not styles

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -39,7 +39,7 @@ Lorem ipsum dolor sit amet, consectetur.</p>
           <div className={styles.description}>
             <h4>Luismar Barazarte</h4>
             <p>Directora de Diseño</p>
-            <ul>
+            <ul className={styles.icons}>
               <li className={styles.facebook}><Facebook fill="#4e8c97" width="13px" height="13px" /></li>
               <li className={styles.x}><X fill="#4e8c97" width="13px" height="13px" /></li>
               <li className={styles.gmail}><Gmail fill="#4e8c97" width="18px" height="18px" /></li>
